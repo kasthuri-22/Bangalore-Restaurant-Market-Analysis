578a483aea3d47e1448472e7807836b05ca65570
KASTHURI S <kastuhh22@gmail.com>
2026-07-09 20:46:39 +0530

    Update 04_Data_Cleaning.sql

--- 05_SQL_Analysis/04_Data_Cleaning.sql
+++ 05_SQL_Analysis/04_Data_Cleaning.sql
@@ -1,1 +1,10 @@
+-- =====================================================
+-- Task 1: Create a working copy of the raw dataset
+-- Purpose:
+-- Preserve the original dataset and perform all cleaning
+-- operations on a separate table.
+-- =====================================================
 
+CREATE TABLE zomato_cleaned AS
+SELECT *
+FROM zomato;
